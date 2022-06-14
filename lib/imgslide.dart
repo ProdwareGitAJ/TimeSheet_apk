4325ecf02969a75ecb49d73666784105543a3701
ProdwareGitAJ <ajoshi@prodwaresol.com>
2022-06-14 05:42:28 -0400

    fc

---
+++ lib/imgslide.dart
@@ -0,0 +1,63 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class slideimg extends StatelessWidget {
+  const slideimg({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Image> img;
+    return (Scaffold(
+      body: Container(
+        child: Center(
+            child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: SizedBox(
+                  height: 225,
+                  width: double.infinity,
+                  child: CarouselSlider(
+                    items: img = [
+                      Image.asset(
+                        "images/slide1.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        "images/slide2.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        "images/slide3.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        "images/slide1.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        "images/slide2.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        "images/slide3.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        "images/slide1.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      autoPlayAnimationDuration: Duration(milliseconds: 1200),
+                    ),
+                  )),
+            )
+          ],
+        )),
+      ),
+    ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_prj_1/imgslide.dart';
+
+void main() {
+  runApp(jola());
+}
+
+class jola extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.power_settings_new),
+            )
+          ],
+        ),
+        body: slideimg(),
+        drawer: Container(
+          child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Menu bar',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 211, 12, 12),
+                        fontSize: 34,
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text('Messages'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_circle),
+                  title: Text('Profile'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.add_reaction_sharp),
+                  title: Text('Employee'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
